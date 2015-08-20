120d7c02eafef587fe8251743e93670af833c3e2
Robert Egorov <robert.egorov@gmail.com>
2015-08-21 00:07:23 +0200

    version 0.0.2

--- logwriter.go
+++ logwriter.go
@@ -1,4 +1,4 @@
-// Package logwriter automates routine related to log file generation.
+// Package logwriter automates log file writing routines.
 package logwriter
 
 import (
@@ -10,61 +10,96 @@ import (
 	"time"
 )
 
-// Log file switching rules
-const (
-	SwitchByDuration = 1 << iota
-	SwitchBySize
-	SwitchAtMidnight
-	SwitchByStart
+var (
+	// Default extension for log files. Change it if required.
+	LogFileExtension string = ".log"
+
+	// Default extension for trace files. Change it if required.
+	TraceFileExtension string = ".trc"
 )
 
 // RunningMode specifies application running mode
 type RunningMode int
 
 const (
-	DebugMode      RunningMode = 0 // Writes into file and os.Stdout
-	ProductionMode RunningMode = 1 // Writes into file
+	// Writes into file and os.Stdout
+	DebugMode RunningMode = 0
+
+	// Writes into file
+	ProductionMode RunningMode = 1
 )
 
-// Postfixes of environment variables
-const (
-	EnvDeploymentType = "_deployment_type"
-	EnvLoggingLevel   = "_logging_level"
-)
+// Config stores LogWriter parameters
+type Config struct {
+	// Current running mode
+	Mode RunningMode
 
-const logFileExtension = ".log"
+	// Output buffer size
+	BufferSize int
 
-// LogWriter is a special writer helping to resolve logging routines
+	// Buffer flush to HDD every
+	BufferFlushInterval time.Duration
+
+	// Create new log file if existing size is over
+	SwitchSize int64
+
+	// Create new log file every SwitchInterval (after the 1st log item arrival)
+	SwitchInterval time.Duration
+
+	// Create new log file at midnight
+	SwitchAtMidnight bool
+
+	// Where to create active log file
+	Path string
+
+	// Where to copy log file after 'switching'
+	ArchivePath string
+}
+
+
+// LogWriter wraps io.Writer to automate routine with log files
 type LogWriter struct {
-	io.Writer
+	w io.Writer
 	sync.RWMutex
-	LogWriterConfig
-	uid          string
-	f            *os.File
-	midnigth     time.Time // Time of first succefull Write call
-	total        int64     // Total # of bytes transferred
-	fileName     string    // имя файла
-	switchTime   time.Time
-	tmpFullName  string
-	archFullName string
-	tmpName      string
+
+	// LW instance configration
+	config Config
+
+	buf []byte
+	buftotal int 
+
+	uid string
+	f   *os.File
+
+	// coming midnight
+	midnigth time.Time
+
+	// active log file length
+	total int64
+
+	// active log file name (usually $uid.log)
+	fileName string
+
+	// time of next log file swithing. If IsZero() == true, feature not used
+	switchTime time.Time
 }
 
-type LogWriterConfig struct {
-	Mode        RunningMode
-	Path        string        // каталог для текущего лог файла
-	MaxSize     int64         // макс размер лог файла в байтах
-	MaxDuration time.Duration // макс длительность накоплений данных
-	ArchivePath string        // каталог для архивных файлов
-}
 
-// NewLogWriter creates new LogWriter, opens log file
-func NewLogWriter(uid string, lwc LogWriterConfig) (lw *LogWriter, err error) {
+// NewLogWriter creates new LogWriter and main log file
+func NewLogWriter(uid string, cfg *Config) (lw *LogWriter, err error) {
 
 	lw = &LogWriter{
-		uid:             uid,
-		LogWriterConfig: lwc,
-		RWMutex:         sync.RWMutex{}}
+		uid:     uid,
+		RWMutex: sync.RWMutex{}}
+
+	if cfg != nil {
+		lw.config = *cfg
+	}
+
+	if lw.config.BufferSize > 0 {
+		// reserve double len
+		lw.buf = make([]byte, cfg.BufferSize*2)
+	}
 
 	if err := lw.createLogFile(); err != nil {
 		return nil, err
@@ -73,150 +108,193 @@ func NewLogWriter(uid string, lwc LogWriterConfig) (lw *LogWriter, err error) {
 	return lw, nil
 }
 
-func (lw *LogWriter) SetConfig(mode RunningMode) (err error) {
-	return lw.setMode(mode)
-}
-
-func (lw *LogWriter) SetMode(mode RunningMode) (err error) {
-	return lw.setMode(mode)
-}
-
-func (lw *LogWriter) setMode(mode RunningMode) (err error) {
-
+// SetConfig updates LogWriter config parameters
+func (lw *LogWriter)SetConfig(cfg *Config) {
+	
 	lw.Lock()
-	if lw.Mode == mode {
-		lw.Unlock()
+	if cfg == nil {
+		lw.setConfig(&Config{})
+	} else {
+		lw.setConfig(cfg)
+	}
+	lw.Unlock()
+	
+	return
+}
+
+
+func (lw *LogWriter)setConfig(cfg *Config) {
+	
+	oldmode := lw.config.Mode
+	lw.config = *cfg
+	
+	if  oldmode != cfg.Mode {
+		lw.setMode(cfg.Mode)
+	}
+		
+	return
+}
+
+// Close flushes file buffer and closes log file
+func (lw *LogWriter) Close() (err error) {
+	lw.Lock()
+	err = lw.close()
+	lw.Unlock()
+	return
+}
+
+func (lw *LogWriter) close() (err error) {
+
+	if lw.f == nil {
 		return nil
 	}
 
+	// TODO: flushbuffer
+
+	return lw.f.Close()
+}
+
+func (lw *LogWriter) SetMode(mode RunningMode) {
+	lw.Lock()
+	lw.setMode(mode)
+	lw.Unlock()
+}
+
+func (lw *LogWriter) setMode(mode RunningMode) {
+
+	
 	if mode == DebugMode {
 		if lw.f != nil {
-			lw.Writer = io.MultiWriter(lw.f, os.Stdout)
+			lw.w = io.MultiWriter(lw.f, os.Stdout)
 		} else {
-			lw.Writer = os.Stdout
+			lw.w = os.Stdout
 		}
 	} else if mode == ProductionMode {
 		if lw.f != nil {
-			fstat, err := lw.f.Stat()
-			if err == nil {
-				lw.Writer = lw.f
-				lw.total = fstat.Size()
-			}
+			lw.w = lw.f
 		} else {
-			lw.Writer = os.Stderr
+			lw.w = os.Stderr
 		}
 	}
+	
+	lw.config.Mode = mode
 
-	lw.Unlock()
-	return err
+
+	return
 }
 
 // Write 'overrides' the underlying io.Writer's Write method.
-// This is the one that will be called by io.Copy(). We simply
-// use it to keep track of byte counts and then forward the call.
 func (lw *LogWriter) Write(p []byte) (n int, err error) {
 
+	// TODO: buffered i/o
+	if len(p) == 0 {
+		return 0, nil
+	}
+	
 	lw.Lock()
-
-	n, err = lw.Writer.Write(p)
+	
+	
+	if len(lw.buf) > 0 {
+		if 	len(p) + lw.buftotal < len(lw.buf)/2 {
+			copy(lw.buf[lw.buftotal:], p)
+			lw.buftotal += len(p)
+			lw.Unlock()
+			return len(p), nil
+		} else {
+			p = lw.buf[:lw.buftotal]
+			lw.buftotal = 0
+		}
+	} 
+	
+	 
+	
+	n, err = lw.w.Write(p)
 
 	if err != nil {
 		lw.Unlock()
-		panic(err)
 		return n, err
-	}
+	}	
 
 	lw.total += int64(n)
+		
+	doSwitch := (lw.config.SwitchSize > 0 && lw.config.SwitchSize < lw.total) ||
+		(lw.config.SwitchInterval != 0 && !lw.switchTime.IsZero() && time.Now().After(lw.switchTime))
 
-	t := time.Now()
-
-	if lw.MaxSize > 0 && lw.MaxSize < lw.total { // если активен режим разбиения файлов по размеру и размер файла превысил лимит
-
-		err = lw.switchFile()
-
-	} else if lw.MaxDuration != 0 {
-		// если активен режим разбиения файлов по периоду времени
-
+	if !doSwitch {
 		if lw.switchTime.IsZero() {
-			lw.switchTime = t.Add(lw.MaxDuration)
-		} else if t.After(lw.switchTime) {
-			err = lw.switchFile()
+			lw.switchTime = time.Now().Add(lw.config.SwitchInterval)
 		}
-	} else if t.After(lw.midnigth) {
-		err = lw.switchFile()
+
+		doSwitch = lw.config.SwitchAtMidnight && time.Now().After(lw.midnigth)
 	}
 
-	lw.Unlock()
-	if err != nil {
-		panic(err)
+	if doSwitch {
+		err = lw.switchFile()
 	}
+	
+	
+	lw.Unlock()
 
 	return n, err
-
 }
 
-func (lw *LogWriter) SwitchFile() (err error) {
-
+// ForceSwitchFile immediately archives active log file
+func (lw *LogWriter) ForceSwitchFile() (err error) {
 	lw.Lock()
 	err = lw.switchFile()
 	lw.Unlock()
 	return err
 }
 
+//
 func (lw *LogWriter) switchFile() error {
 
-	// закрываем файл если открыт
+	// close() file if it's open
 	if lw.f != nil {
 		if err := lw.f.Close(); err != nil {
-			panic(err)
 			return err
 		}
 	}
 
-	lw.tmpName = fmt.Sprintf("%s-%d", lw.uid, time.Now().UnixNano()) // Format("2006-01-02T15-04-05-.000000")
-	lw.tmpFullName = filepath.Join(lw.Path, lw.tmpName)
+	tmpName := fmt.Sprintf("%s-%d", lw.uid, time.Now().UnixNano()) // Format("2006-01-02T15-04-05-.000000")
+	tmpFullName := filepath.Join(lw.config.Path, tmpName)
 
-	if err := os.Rename(lw.f.Name(), lw.tmpFullName); err != nil {
+	if err := os.Rename(lw.f.Name(), tmpFullName); err != nil {
 		return err
 	}
 
-	// копируем файл в другом потоке
-	lw.archFullName = filepath.Join(lw.ArchivePath, lw.tmpName+logFileExtension)
+	archFullName := filepath.Join(lw.config.ArchivePath, tmpName+LogFileExtension)
 
+	// rename (probably copy file) in parallel routine
 	go func(t, a string) {
 		if err := os.Rename(t, a); err != nil {
-			panic(err)
 			lw.Write([]byte("\nLog file archiving error!\n" + err.Error()))
 		}
-	}(lw.tmpFullName, lw.archFullName)
+	}(tmpFullName, archFullName)
 
 	return lw.createLogFile()
 }
 
+// Creates active log file "$uid.log"
 func (lw *LogWriter) createLogFile() (err error) {
 
-	lw.f, err = os.OpenFile(filepath.Join(lw.Path, lw.uid+logFileExtension), os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+	lw.f, err = os.OpenFile(filepath.Join(lw.config.Path, lw.uid+LogFileExtension), os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
 
-	if err == nil {
-		lw.total = 0
-		lw.midnigth = time.Now().Truncate(24 * time.Hour).Add(24 * time.Hour)
-		lw.switchTime = time.Time{}
+	if err != nil {
+		return err
 	}
 
-	lw.Writer = lw.f
+	fstat, err := lw.f.Stat()
+	if err != nil {
+		return err
+	}
 
-	return err
-}
+	lw.total = fstat.Size()
+	lw.midnigth = time.Now().Truncate(24 * time.Hour).Add(24 * time.Hour)
+	lw.switchTime = time.Time{}
 
-func (l *LogWriter) renameFile() error {
+	lw.setMode(lw.config.Mode)
 
 	return nil
-}
 
-func (l *LogWriter) readyToArchive(total int64) bool {
-
-	// TODO:
-	// проверять время с последней записи
-	// а так же
-	return false
 }
--- logwriter_test.go
+++ logwriter_test.go
@@ -1,25 +1,28 @@
 package logwriter_test
 
 import (
-	"github.com/regorov/logwriter"
 	"log"
 	"os"
 	"testing"
-	"time"
+	_ "time"
+
+	"github.com/regorov/logwriter"
 )
 
-func BenchmarkWriteWitoutMutex(b *testing.B) {
+// Replace value with smth close to your typical log item
+var TypicalLogItem string = "logwriter2015/08/17 00:33:00 Test call 48117\n"
 
-	f, err := os.OpenFile("withoutmutex.log", os.O_CREATE|os.O_RDWR, 0)
+// Direct non-buffered file write
+func BenchmarkDirectFileWrite(b *testing.B) {
+
+	f, err := os.OpenFile("directfilewrite.log", os.O_CREATE|os.O_RDWR, 0)
 	if err != nil {
 		b.Fatal(err)
 	}
 
-	l := log.New(f, "logwriter", log.Ldate|log.Ltime)
-
 	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
-		l.Printf("Test call %d", i)
+		f.Write([]byte(TypicalLogItem))
 	}
 
 	f.Close()
@@ -27,18 +30,65 @@ func BenchmarkWriteWitoutMutex(b *testing.B) {
 	return
 }
 
-func BenchmarkWrite(b *testing.B) {
+// Write into the file by standard log package using log.Output()
+func BenchmarkStandardLogToFileUsingOutput(b *testing.B) {
 
-	lw, err := logwriter.NewLogWriter("withmutex", logwriter.LogWriterConfig{MaxDuration: 200 * time.Millisecond, ArchivePath: ""}) //maxSize: 1024 * 1024})
+	f, err := os.OpenFile("standardlog-output.log", os.O_CREATE|os.O_RDWR, 0)
 	if err != nil {
 		b.Fatal(err)
 	}
 
-	l := log.New(lw, "logwriter", log.Ldate|log.Ltime)
+	// no prefix and date format to reduce extra work
+	l := log.New(f, "", 0)
 
 	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
-		l.Printf("Test call %d", i)
+		l.Output(2, TypicalLogItem)
+	}
+
+	f.Close()
+
+	return
+}
+
+// Write into the file by standard log package
+func BenchmarkStandardLogToFileUsingPrint(b *testing.B) {
+
+	f, err := os.OpenFile("standardlog-print.log", os.O_CREATE|os.O_RDWR, 0)
+	if err != nil {
+		b.Fatal(err)
+	}
+
+	// no prefix and date format to reduce extra work
+	l := log.New(f, "", 0)
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		l.Print(TypicalLogItem)
+	}
+
+	f.Close()
+
+	return
+}
+
+//
+func BenchmarkLogWrite(b *testing.B) {
+
+	lw, err := logwriter.NewLogWriter("logwriter", &logwriter.Config{BufferSize: 1024*1024, ArchivePath: "", Mode: logwriter.ProductionMode}) //maxSize: 1024 * 1024})
+	if err != nil {
+		b.Fatal(err)
+	}
+
+	l := log.New(lw, "", 0)
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		l.Print(TypicalLogItem)
+	}
+
+	if err := lw.Close(); err != nil {
+		b.Fatal(err)
 	}
 
 	return
